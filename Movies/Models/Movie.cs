06a83c5b71733bd9eb4220a2619aa15c6eeb391f
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-10-17 00:09:40 +1000

    Blazor Movies DONE

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+	public class Movie
+	{
+		public int id { get; set; }
+		[Required] // аннотации
+		[StringLength(32, MinimumLength =2)]
+		[DisplayName("Название")]
+		public string Title { get; set; }
+
+		[DisplayName("Дата выхода")]
+		[Range(typeof(DateOnly), "1895-10-14", "2032-12-31")]
+		public DateOnly ReleaseDate { get; set; }
+
+		[DisplayName("Жанр")]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+		public string? Genre { get; set; }
+
+		[DisplayName("Цена")]
+		public decimal? Price { get; set; }
+	}
+}
