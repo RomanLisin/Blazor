8a3a3dac42efe4d1cb0b2425e6f851982d9b0c17
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-10-18 00:45:50 +1000

    Add Students

---
+++ Academy/Models/Discipline.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+	public class Discipline
+	{
+		[Key]
+		
+		public short discipline_id { get; set; }
+		[Required]
+		public string discipline_name { get; set; } = string.Empty;
+		[Required]
+
+
+		[Column(TypeName = "tinyint")]
+		public short number_of_lessons { get; set; }
+	}
+}
---
+++ Academy/Models/Students.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Academy.Models
+{
+	public class Students
+	{
+		[Key]
+		[Column(TypeName = "int")]
+		public short stud_id { get; set; }
+
+		[Required]
+		public string last_name { get; set; } = string.Empty;
+		[Required]
+		public string first_name { get; set; } = string.Empty;
+		public string? middle_name { get; set; } = string.Empty;
+		[Required]
+		public DateOnly? birth_date { get; set; }
+		public string? email { get; set; } = string.Empty;
+		public string? phone { get; set; } = string.Empty;
+		public byte[]? photo { get; set; } // ? - так как это поле в базе может быть NULL
+
+		public int? group { get; set; }
+
+
+	}
+}
